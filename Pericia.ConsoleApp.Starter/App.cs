d7ebf6579fe8a2284ab4c5e4e8050f0d708c3970
Guillaume Lacasa <github@adhess.net>
2021-01-05 18:35:19 +0100

    Add project files.

---
+++ Pericia.ConsoleApp.Starter/App.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pericia.ConsoleApp.Starter
+{
+    public class App
+    {
+        private readonly ILogger logger;
+
+        public App(ILogger<App> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task Run()
+        {
+            logger.LogInformation("Hello World!");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Pericia.ConsoleApp.Starter/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Pericia.ConsoleApp.Starter;
+using System.IO;
+
+
+var builder = new HostBuilder()
+    .UseContentRoot(Directory.GetCurrentDirectory())
+    .ConfigureHostConfiguration(builder=> {
+        builder.AddCommandLine(args);
+    })
+    .ConfigureAppConfiguration((ctx, builder) =>
+    {
+        builder.AddJsonFile("appsettings.json")
+               .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: true);
+
+        if (ctx.HostingEnvironment.IsDevelopment())
+        {
+            builder.AddUserSecrets<App>();
+        }
+    })
+    .ConfigureLogging((ctx, logging) =>
+    {
+        logging.AddConsole();
+    })
+    .ConfigureServices((ctx, services) =>
+    {
+        services.AddTransient<App>();
+    })
+    .UseConsoleLifetime();
+
+var host = builder.Build();
+
+using (var servicesScope = host.Services.CreateScope())
+{
+    var app = servicesScope.ServiceProvider.GetRequiredService<App>();
+    await app.Run();
+}
